7b8704e494e1b212d484592647e91045cb177fc9
Albert Tang <tangalbert919@yahoo.com>
2023-05-11 15:17:17 -0500

    NX669J: Build boot control HAL
    Change-Id: I96fdd1317b09e164a9ade1eedd5ac2b6e8811ec8

--- device.mk
+++ device.mk
@@ -121,6 +121,12 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
