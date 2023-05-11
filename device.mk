e2f12b6ef2694d88c6c6b9479440eae74876c2ca
Albert Tang <tangalbert919@yahoo.com>
2023-05-11 13:08:45 -0500

    NX669J: Execute A/B post-install scripts
    Change-Id: If086cd5ce641dc1a084d55d261a17b3c42866ffd

--- device.mk
+++ device.mk
@@ -48,6 +48,23 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage \
     $(LOCAL_PATH)/overlay-system
 
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
