2127291ffd2a8cdc6ae6c5f5d7dff362fa052a2e
Albert Tang <tangalbert919@yahoo.com>
2023-05-11 13:14:37 -0500

    NX669J: Build Audio HAL V6, Sound Trigger V2.3
    Change-Id: I859c67bd79b74755302455afbe61d5f538b9a76e

--- device.mk
+++ device.mk
@@ -67,13 +67,13 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio.common@2.0-util \
-    android.hardware.audio.common@5.0-util \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.common@6.0-util \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.default \
